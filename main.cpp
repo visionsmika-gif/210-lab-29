5a91abfb776e6ae0a0460b3f4b8a7d3b90e5b128
visionsmika-gif <visionsmika@gmail.com>
2025-11-04 15:07:33 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* INSTRUCTIONS:
+
+The pseudocode will be coded using VS Code connected to a GitHub repository.
+
+Pseudocode is plain-language comments, placed in your code as actual comments, that serve as a coding framework.
+What are the major steps and blocks of code necessary for the project? What functions? Put placeholders for all
+those with pseudocode comments.
+
+Write a mockup, which is a bare-bones wireframe of your code. It should have dummy variables and do trivial
+outputs to demonstrate that the functions work, are passing data, and that the data structure is coded properly
+with one data element. */
+
+// Pseudocode and mockup:
