cc7d13acb141ccd46b87db5b3b185eb0dfcda062
visionsmika-gif <visionsmika@gmail.com>
2025-11-06 22:29:02 -0800

    update

--- main.cpp
+++ main.cpp
@@ -163,19 +163,20 @@ int main() {
 	return 0;
 }
 
-
+// Function to generate a random number between MIN and MAX inclusive.
 int generateRandomNum(const int MIN, const int MAX) {
 	return MIN + (rand() % (MAX - MIN + 1));
 }
 
+// Function to add a certain number of clothes to a given category in a store.
 void addClothes(const int CAT_INDEX, const int NUM_CLOTHES, array<list<Clothing>, NUM_CATEGORIES>& store, const array<vector<Clothing>, NUM_CATEGORIES>& clothingPool) {
 	for (int i = 0; i < NUM_CLOTHES; ++i) {
 		int index = generateRandomNum(0, (clothingPool[CAT_INDEX].size() - 1));
-		// cout << "Adding " << categoryNames[CAT_INDEX] << ".\n";			DELETE THIS LINE. is used for debug purposes
 		store[CAT_INDEX].push_back(clothingPool[CAT_INDEX].at(index));
 	}
 }
 
+// Function to read pieces of clothing from a file and store it into the vector clothingPool.
 void populateClothingPool(array<vector<Clothing>, NUM_CATEGORIES>& clothingPool, const string FILE_NAME) {
 	ifstream clothingFile;
 	clothingFile.open(FILE_NAME);
@@ -204,9 +205,7 @@ void populateClothingPool(array<vector<Clothing>, NUM_CATEGORIES>& clothingPool,
 	clothingFile.close();
 }
 
-// displayStock() - Function to display store stock
-// Arguments: a map of clothing stores.
-// Goes through each map element, outputs the name of the store (map key) and the clothing stores' tops, bottoms, and shoes (map value)
+// Function to display the stock of all clothing stores.
 void displayStock(const map<string, array<list<Clothing>, NUM_CATEGORIES>>& clothingStores) {
 	// For every store,
 	for (const auto& store : clothingStores) {
@@ -230,9 +229,7 @@ void displayStock(const map<string, array<list<Clothing>, NUM_CATEGORIES>>& clot
 	}
 }
 
-// restockClothing() - Function to add new clothing to a particular store
-// Arguments: a map of clothing stores, the name of the particular clothing store, the three vectors for tops, bottoms, and shoes.
-// One random category (tops, bottoms, shoes) is selected restocked with a random number of clothes
+// Function to add new clothing to a particular store.
 void restockClothing(map<string, array<list<Clothing>, NUM_CATEGORIES>>& clothingStores, const string& storeName,
 	const array<vector<Clothing>, NUM_CATEGORIES>& clothingPool) {
 
@@ -242,20 +239,17 @@ void restockClothing(map<string, array<list<Clothing>, NUM_CATEGORIES>>& clothin
 	// Choose a random number of clothes to add to that category.
 	const int MIN = 1;
 	const int MAX = 3;
-	int numClothes = MIN + (rand() % (MAX - MIN + 1));	// Random number between 1-3
+	int numClothes = generateRandomNum(MIN, MAX);
 
+	// Output action.
 	cout << numClothes << " " << categoryNames[catIndex] << " get restocked\n";
-
 
 	// Add clothes to that category.
 	auto& store = clothingStores[storeName];
 	addClothes(catIndex, numClothes, store, clothingPool);
 }
 
-// sellClothing() - Function to sell clothing from a particular store
-// Arguments - a map of clothing stores, the name of the particular clothing store.
-// One random category (tops, bottoms, shoes) is selected and guaranteed to sell a random number of clothes
-// The other categories have a chance to sell
+// Function to sell clothing from a particular store.
 bool sellClothing(map<string, array<list<Clothing>, NUM_CATEGORIES>>& clothingStores, const string& storeName) {
 
 	// Choose a random category to sell from.
@@ -267,35 +261,33 @@ bool sellClothing(map<string, array<list<Clothing>, NUM_CATEGORIES>>& clothingSt
 		return false;	// False - nothing sold
 	}
 
-	// Choose a random number of clothes to sell from category.	
+	// Choose a random number of clothes to sell from that category.
 	const int MIN = 1;
 	const int MAX = 3;
 	int randomNum = generateRandomNum(1, 3);
 
-	// Ensure we don't sell more clothes than we have
+	// Ensure we don't sell more clothes than we have.
 	while (randomNum > store[catIndex].size()) {
 		randomNum = generateRandomNum(1, 3);
 	}
 
+	// Output action.
 	cout << randomNum << " " << categoryNames[catIndex] << " get sold\n";
 
 	// Sell clothes from that category.
 	for (int i = 0; i < randomNum; ++i) {
-		// Select a random clothing to sell
+		// Select a random clothing piece to sell.
 		auto it = store[catIndex].begin();
 		int position = generateRandomNum(0, (store[catIndex].size() - 1));
 		advance(it, position);
 
-		// Sell that particular clothing
+		// Sell that clothing piece.
 		store[catIndex].erase(it);
 	}
-	return true;	// true - something sold
+	return true;	// True - something sold
 }
 
-// transferClothing() - Function to transfer clothing between two stores
-// Arguments - a map of clothing stores, the name of the particular clothing store
-// A random other store is chosen to be transferred to
-// A random piece of clothing from a random category is taken from the parameter store and added to the other store
+// Function to transfer clothing from one store to another.
 bool transferClothing(map<string, array<list<Clothing>, NUM_CATEGORIES>>& clothingStores, const string& storeName) {
 
 	// Choose a random category to transfer.
@@ -323,26 +315,27 @@ bool transferClothing(map<string, array<list<Clothing>, NUM_CATEGORIES>>& clothi
 		int position = generateRandomNum(0, clothingStores.size() - 1);
 		otherStoreIt = clothingStores.begin();
 		advance(otherStoreIt, position);
-	} while (otherStoreIt->first == storeName);
+	} while (otherStoreIt->first == storeName);		// Ensure that the store doesn't transfer to itself.
 
 	// Output action.
 	if (otherStoreIt != clothingStores.end()) {
 		cout << storeName << " transfers " << numClothes << " " << categoryNames[catIndex] << " to " << otherStoreIt->first << ".\n";
 	}
 
-	// Remove clothes from one store and add them to the other.
+	// Remove clothes from this store and add them to the other.
 	for (int i = 0; i < numClothes; ++i) {
-		// Select a random clothing to transfer
+		// Select a random clothing piece to transfer.
 		auto it = thisStore[catIndex].begin();
 		int position = generateRandomNum(0, (thisStore[catIndex].size() - 1));
 		advance(it, position);
 
-		// Save that clothing
+		// Save that clothing piece.
 		Clothing transferredPiece = *it;
 
-		// Remove that particular clothing from our store
+		// Remove that clothing piece from this store.
 		thisStore[catIndex].erase(it);
 
+		// Add that clothing piece to the other store.
 		otherStoreIt->second[catIndex].push_back(transferredPiece);
 	}
 
